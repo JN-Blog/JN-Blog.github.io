693658002ba76f4033e72fdc26a80f5df58df5ab
juliennuellas <julien.nuellas@gmail.com>
2018-11-04 16:48:58 +0100

    reorganized themes folder

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ PYGMENTS_STYLE = 'monokai'
 # THEME = "../JN-PelicanTheme-Minimalist/Responsive-Pelican"
 
 # CSS_FILE = 'style.css'
-THEME = "themes/Flex"
+THEME = "themes/dist"
 PATH = 'content'
 STATIC_PATHS = [
     'images',
