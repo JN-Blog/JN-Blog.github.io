f18e6b2f713309bf7fafe6dfb9dd6752da06d20e
juliennuellas <julien.nuellas@gmail.com>
2018-12-20 19:44:59 +0100

    quelques adapatation à faire sur le header pour mieux aligner le titre avec les articles (responsive-: a tester dans themes/src

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ PYGMENTS_STYLE = 'monokai'
 # THEME = "../JN-PelicanTheme-Minimalist/Responsive-Pelican"
 
 # CSS_FILE = 'style.css'
-THEME = "themes/dist"
+THEME = "themes/src"
 PATH = 'content'
 STATIC_PATHS = [
     'images',
