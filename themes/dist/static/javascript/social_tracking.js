a406944e338aec6d9d84c58ad506e72f850dd948
juliennuellas <julien.nuellas@gmail.com>
2018-12-27 19:58:50 +0100

    en cours d'identification du probleme de tracking ga sur les events social

--- themes/dist/static/javascript/social_tracking.js
+++ themes/dist/static/javascript/social_tracking.js
@@ -3,21 +3,18 @@ class SocialButton {
     constructor(name) {
         this.name = name;
         this.button = document.getElementById(this.name);
-        this.hitType = 'social';
-        this.socialAction = "Share";
         this.socialTarget = document.location.href;
         this.trackShareAction(this.button);
     }
 
     trackShareAction(buttonElt) {
         buttonElt.addEventListener("click", () => {
-            ga('send', {
-                hitType: 'social',
+            ga('send', 'social', {
                 socialNetwork: buttonElt.name,
                 socialAction: 'Share',
                 socialTarget: document.location.href
               });
-        })
+        });
     }
 
 }
